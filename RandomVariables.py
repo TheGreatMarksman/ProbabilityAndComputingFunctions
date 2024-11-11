1e7ae4a0d422f69d7d88f22bc1049eb3386cf6e1
TheGreatMarksman <marcusedooley@yahoo.com>
2024-11-10 21:12:39 -0800

    finished distributions and adding testing functions

--- RandomVariables.py
+++ RandomVariables.py
@@ -1,3 +1,5 @@
+import math
+
 # Counts the number of heads of three coins tossed
 class NumHeads:
     def __init__(self):
@@ -23,25 +25,34 @@ class NumHeads:
         return event
     def probability_of(self, event):
         return event.__len__() / self.sample_space.__len__()
-    def range(self):
+    def get_range(self):
         RV_range = []
         for i in self.sample_space:
             result = self.evaluate_at(i)
             if not RV_range.__contains__(result):
                 RV_range.append(result)
         return RV_range
+    @staticmethod
+    def test():
+        RV = NumHeads()
+        print(f"Let A = the number of heads that show up when 3 coins are tossed")
+        print(f"A([True, True, False]): {RV.evaluate_at([True, True, False])}")
+        print(f"A = 2: {RV.equals(2)}")
+    
+        print(f"PDF(2) = {PDF(2, RV)}")
+        print(f"CDF(2.3) = {CDF(2.3, RV)}")
 
 # L14-4
-# Returns if a biased coin comes up heads
+# Equals 1 if a biased coin comes up heads and 0 otherwise
 class IsHeads:
     def __init__(self, probability):
         self.sample_space = self.make_sample_space()
         self.probability = probability
     def make_sample_space(self):
-        sample_space = [0, 1]
+        sample_space = [[0], [1]]
         return sample_space
     def evaluate_at(self, event):
-        return event
+        return event[0]
     def equals(self, num):
         event = []
         for i in self.sample_space:
@@ -49,19 +60,28 @@ class IsHeads:
                 event.append(i)
         return event
     def probability_of(self, event):
-        if event == 1:
+        if event == [[1]]:
             return self.probability
-        elif event == 0:
+        elif event == [[0]]:
             return 1 - self.probability
         else:
             return 10
-    def range(self):
+    def get_range(self):
         RV_range = []
         for i in self.sample_space:
             result = self.evaluate_at(i)
             if not RV_range.__contains__(result):
                 RV_range.append(result)
         return RV_range
+    @staticmethod
+    def test(p):
+        RV = IsHeads(p)
+        print(f"Let A = 1 if a coin is heads and 0 otherwise. Biased coin has probability {p} to get heads")
+        print(f"A([1]): {RV.evaluate_at([1])}")
+        print(f"A = 0: {RV.equals(0)}")
+    
+        print(f"PDF(1) = {PDF(1, RV)}")
+        print(f"CDF(0.5) = {CDF(0.5, RV)}")
 
 # L14-5
 # Returns number on die rolled
@@ -84,40 +104,65 @@ class NumberRolled:
             return 1/6
         return 0
 
-    def range(self):
+    def get_range(self):
         RV_range = []
         for i in range(1, 6):
             RV_range.append(i)
         return RV_range
 
 # L14-6
-# Counts the number of heads in the 3 coin tosses each with probability p
+# Counts the number of heads in the n = 3 coin tosses each with probability p
 class NumBiasedHeads:
     def __init__(self, p):
         self.sample_space = self.make_sample_space()
         self.p = p
+        self.n = 3
     def make_sample_space(self):
         sample_space = [[0, 0, 0], [0, 0, 1], [0, 1, 1], [0, 1, 0], [1, 1, 0], [1, 0, 0],
                         [1, 0, 1], [1, 1, 1]]
         return sample_space
-    def evaluate_at(self, n):
-        # finish later
-        return n
+    def evaluate_at(self, event):
+        return event.count(1)
     def equals(self, num):
-        if num > 0 and num < 7:
-            return [num]
-        return [ 10 ]
+        event = []
+        for i in self.sample_space:
+            if num == self.evaluate_at(i):
+                event.append(i)
+        return event
     def probability_of(self, event):
-        if event[0] > 0 and event[0] < 7:
-            return 1/6
-        return 0
+        num = self.evaluate_at(event)
+        return combination(self.n, num) * pow(p, num) * pow(1 - p, self.n - num)
 
-    def range(self):
+    def get_range(self):
         RV_range = []
-        for i in range(1, 6):
-            RV_range.append(i)
+        for i in self.sample_space:
+            result = self.evaluate_at(i)
+            if not RV_range.__contains__(result):
+                RV_range.append(result)
         return RV_range
 
+# L14-8
+# Counts the number of tosses needed to get the first heads
+# Domain consists of numbers from 1 to 10 indicating how many tosses it took
+class NumTossesUntilHead:
+    def __init__(self, p):
+        self.sample_space = self.make_sample_space()
+        self.p = p
+        self.MAX_TOSSES = 10
+    def make_sample_space(self):
+        sample_space = []
+        for i in range(1, self.MAX_TOSSES + 1):
+            sample_space.append(i)
+        return sample_space
+    def evaluate_at(self, event):
+        return event
+    def equals(self, num):
+        return num
+    def probability_of(self, event):
+        return pow(1 - p, event - 1) * p
+
+    def get_range(self):
+        return self.make_sample_space()
 
 # Incomplete
 def make_sample_space(num_coins):
@@ -154,13 +199,13 @@ def combination(n, r):
 
 def factorial(num):
     result = 1
-    for i in range(1, num):
+    for i in range(1, num + 1):
         result *= i
     return result
 
-# L12-7
+# L13-5
 def PDF(x, random_variable):
-    RV_range = random_variable.range()
+    RV_range = random_variable.get_range()
     if(RV_range.__contains__(x)):
         return random_variable.probability_of(random_variable.equals(x))
     else:
@@ -168,17 +213,11 @@ def PDF(x, random_variable):
     
 def CDF(x, random_variable):
     sum = 0
-    RV_range = random_variable.range()
+    RV_range = random_variable.get_range()
     for i in RV_range:
         if i < x:
             sum += random_variable.probability_of(random_variable.equals(i))
     return sum
-  
+
 if __name__ == "__main__":
-    RV = NumHeads()
-    print(f"Let A = the number of heads that show up when 3 coins are tossed")
-    print(f"A([True, True, False]): {RV.evaluate_at([True, True, False])}")
-    print(f"A = 2: {RV.equals(2)}")
-    
-    print(f"PDF(2) = {PDF(2, RV)}")
-    print(f"CDF(2.3) = {CDF(2.3, RV)}")
+    IsHeads.test(0.7)
